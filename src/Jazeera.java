51ebeaa4952520526dcc9a6cf7958b86fcd78722
JazeeraArinova <canarijaz@gmail.com>
2023-07-20 16:27:27 -0400

    Jazeera's commit2

--- src/Jazeera.java
+++ src/Jazeera.java
@@ -2,6 +2,8 @@ public class Jazeera {
     public static void main(String[] args) {
 
 
-        System.out.println("Jazeera is here");
+        System.out.println("Jazeera is here1");
+        System.out.println("Jazeera is here2");
+
     }
 }
